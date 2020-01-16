15608897553be536a4bc7b5a32519eac65b2d80e
Vandergav <sohgavin@gmail.com>
2020-01-16 12:42:22 +0800

    Updates

---
+++ precache-manifest.5813b5a56b5ee7b896808db11af1b186.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0582d6e042a95f7649200a3103ac6dd",
+    "url": "/learn-japanese-with-pokemon/index.html"
+  },
+  {
+    "revision": "8b29e7364895e6fd4aba",
+    "url": "/learn-japanese-with-pokemon/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "8bc658c11abc28b0569f",
+    "url": "/learn-japanese-with-pokemon/static/css/main.f678c7ee.chunk.css"
+  },
+  {
+    "revision": "8b29e7364895e6fd4aba",
+    "url": "/learn-japanese-with-pokemon/static/js/2.6974497d.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/learn-japanese-with-pokemon/static/js/2.6974497d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8bc658c11abc28b0569f",
+    "url": "/learn-japanese-with-pokemon/static/js/main.9ea04aea.chunk.js"
+  },
+  {
+    "revision": "7f1a63ef19e9ceafa488",
+    "url": "/learn-japanese-with-pokemon/static/js/runtime-main.f522ed21.js"
+  }
+]);
